1000ba8015e6ab7f5ed85e8f8618d17321e4de21
Nabyeongho <127078321+nabh0203@users.noreply.github.com>
2024-05-20 10:22:32 +0900

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using NewDealMetaverse.Day3.Chapter3;
+using NewDealMetaverse.Day4.Cahptor5;
+using NewDealMetaverse.Day4.Cahptor6;
+using NewDealMetaverse.Day5.Cahptor7;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewDealMetaverse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //WriteResult(new SignedUnsignedEx());
+            //WriteResult(new FloatDoubleEx());
+            //WriteResult(new FloatDoubleEx2());
+            //WriteResult(new CharEx());
+            //WriteResult(new StringEx());
+            //WriteResult(new WhileEx());
+            //WriteResult(new DoWhileEx());
+            //WriteResult(new ForEx());
+            //WriteResult(new BreakWhile());
+            //WriteResult(new BreakFor());
+            //WriteResult(new ContinueEx());
+            //WriteResult(new CalculatorEx());
+            //WriteResult(new SwapbyValueEx());
+            //WriteResult(new OverLoadingEx
+            //WriteResult(new ParamsEx());
+            //WriteResult(new OptionalParameterEx());
+
+            //객체 생성
+            Animal animal = new Animal();
+        }
+
+        private void WriteResult(IPrint result)
+        {
+            txtConsole.Text = result.GetData();
+        }
+    }
+}
